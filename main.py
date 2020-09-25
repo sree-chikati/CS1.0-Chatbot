0f94c025e27d21e4092c3bb0938220622093e965
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-24 19:34:02 -0700

    added if-elif-else statements

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ def get_bot_response(user_response) :
     sweets = ['Ice Cream', 'Chocolate', 'Cake']
     salty = ['Chips', 'Popcorn', 'Cheese & Crackers']
     fruits = ['Bananas', 'Strawberries', 'Grapes']
-    drinks = ['Water', 'Tea', 'Coffe']
+    drinks = ['Water', 'Tea', 'Hot Chocolate']
     quick_meals = ['Mac & Cheese', 'Ramen', 'Salad']
     soups = ['Tomato Soup', 'Chicken Noodle Soup', 'Brocolli Cheddar Soup']
+
+    if user_response == "sweet food":
+        return choice(sweets)
+    elif user_response == "salty food":
+        return choice(salty)
+    elif user_response == "healthy food":
+        return choice(fruits)
+    elif user_response == "beverage":
+        return choice(drinks)
+    elif user_response == "meals":
+        return choice(quick_meals)
+    elif user_response == "soup":
+        return choice(soups)
+    else:
+        return "That's not an option we offer. Please try option or type 'done' to quit."
+    
+
+print("Welcome to the Mood Market Bot Assistant!")
